eaaaad8797028ab2b2fc18af9e13bc246b1e7ebe
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-09-21 06:04:36 +0200

    add screens with animation

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,53 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  void initSlidingAnimation(){
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(minutes: 1),
+    );
+    slidingAnimation = Tween<Offset>(begin: const Offset(0,10), end: Offset.zero).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    Future.delayed(const Duration(seconds: 2),(){
+      Get.to(()=> const HomeView(), transition: Transition.fade, duration: kTransitionDuration);
+    });
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(height: 4),
+        SlidingText(slidingAnimation: slidingAnimation,),
+      ],
+    );
+  }
+}
