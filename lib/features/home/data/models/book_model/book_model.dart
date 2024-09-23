304975784d3e1949a351656760c0ac7df30f545c
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-09-23 05:54:32 +0200

    add domain layer + models

---
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -0,0 +1,63 @@
+import 'package:bookly_app/features/home/data/models/book_model/access_info.dart';
+import 'package:bookly_app/features/home/data/models/book_model/safe_info.dart';
+import 'package:bookly_app/features/home/data/models/book_model/search_info.dart';
+import 'package:bookly_app/features/home/data/models/book_model/volume_info.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+
+class BookModel extends BookEntity {
+  String? kind;
+  String? id;
+  String? etag;
+  String? selfLink;
+  VolumeInfo? volumeInfo;
+  SaleInfo? saleInfo;
+  AccessInfo? accessInfo;
+  SearchInfo? searchInfo;
+
+  BookModel({
+    this.kind,
+    this.id,
+    this.etag,
+    this.selfLink,
+    this.volumeInfo,
+    this.saleInfo,
+    this.accessInfo,
+    this.searchInfo,
+  }) : super(
+      bookId: id!,
+      image: volumeInfo?.imageLinks?.thumbnail ?? '',
+      authorName: volumeInfo?.authors?.first ?? 'No Name',
+      price: 0.0,
+      rating: volumeInfo!.averageRating,
+      title: volumeInfo.title!);
+
+  factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
+    kind: json['kind'] as String?,
+    id: json['id'] as String?,
+    etag: json['etag'] as String?,
+    selfLink: json['selfLink'] as String?,
+    volumeInfo: json['volumeInfo'] == null
+        ? null
+        : VolumeInfo.fromJson(json['volumeInfo'] as Map<String, dynamic>),
+    saleInfo: json['saleInfo'] == null
+        ? null
+        : SaleInfo.fromJson(json['saleInfo'] as Map<String, dynamic>),
+    accessInfo: json['accessInfo'] == null
+        ? null
+        : AccessInfo.fromJson(json['accessInfo'] as Map<String, dynamic>),
+    searchInfo: json['searchInfo'] == null
+        ? null
+        : SearchInfo.fromJson(json['searchInfo'] as Map<String, dynamic>),
+  );
+
+  Map<String, dynamic> toJson() => {
+    'kind': kind,
+    'id': id,
+    'etag': etag,
+    'selfLink': selfLink,
+    'volumeInfo': volumeInfo?.toJson(),
+    'saleInfo': saleInfo?.toJson(),
+    'accessInfo': accessInfo?.toJson(),
+    'searchInfo': searchInfo?.toJson(),
+  };
+}
